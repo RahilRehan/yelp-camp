bcfa8a3a41a7b1688aa8015ce88613dc93befb44
RahilRehan <rahilmuti12345@gmail.com>
2019-09-06 23:14:27 +0530

    Added MongoDB with mongoose
    used a real database instead of an object to store data and hence included data persistence.
    Modules used: mongoose with mongodb

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
-var campgrounds = [
-    {name:"Salmon Creek", image:"https://pixabay.com/get/57e8dc414e5ba814f6da8c7dda793f7f1636dfe2564c704c73287dd3934dc450_340.jpg"},
-    {name:"Granite Hill", image:"https://pixabay.com/get/57e4d64a4a54ad14f6da8c7dda793f7f1636dfe2564c704c73287dd3934dc450_340.jpg"},
-    {name:"Mountain Goat's Rest", image:"https://pixabay.com/get/5fe3dc46425ab108f5d084609620367d1c3ed9e04e50744f72297cd0974ccd_340.jpg"}
-]
+// var campgrounds = [
+//     {name:"Salmon Creek", image:"https://pixabay.com/get/57e8dc414e5ba814f6da8c7dda793f7f1636dfe2564c704c73287dd3934dc450_340.jpg"},
+//     {name:"Granite Hill", image:"https://pixabay.com/get/57e4d64a4a54ad14f6da8c7dda793f7f1636dfe2564c704c73287dd3934dc450_340.jpg"},
+//     {name:"Mountain Goat's Rest", image:"https://pixabay.com/get/5fe3dc46425ab108f5d084609620367d1c3ed9e04e50744f72297cd0974ccd_340.jpg"}
+// ]
+
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+mongoose.connect("mongodb://localhost/campgrounds");
+
+var campgroundSchema = new mongoose.Schema({
+    name:String,
+    image:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name:"Salmon Creek",
+//         image:"https://pixabay.com/get/57e6d7454e53ae14f6da8c7dda793f7f1636dfe2564c704c73287cd3924acd5a_340.jpg"
+//     },
+//     function(err, campground){
+//         if(err){
+//             console.log("Campground not added!");
+//             console.log(err);
+//         }else{
+//             console.log("Campground added!");
+//             console.log(campground);
+//         }
+//     }
+// );
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds",function(req,res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+    Campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds",{campgrounds:campgrounds});
+        }
+    })
 });
 
 app.get("/camgrounds/new", function(req,res){
@@ -26,8 +60,14 @@ app.get("/camgrounds/new", function(req,res){
 
 app.post("/camgrounds", function(req,res){
     var campground = {name:req.body.name, image:req.body.link};
-    campgrounds.push(campground);
-    res.redirect("/campgrounds");
+    Campground.create(campground, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(campground);
+            res.redirect("/campgrounds");
+        }
+    })
 });
 
 app.listen(2900, function(){
