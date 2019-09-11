40213b409b51e9bcf700cf0909ab3991d8ca711e
RahilRehan <rahilmuti12345@gmail.com>
2019-09-11 10:11:06 +0530

    Added models concept
    added model concept, 
    first added campground schema to models folder.

---
+++ models/campground.js
@@ -0,0 +1,11 @@
+import { builtinModules } from "module";
+
+var campgroundSchema = new mongoose.Schema({
+    name:String,
+    image:String,
+    description:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+module.exports = Campground;
